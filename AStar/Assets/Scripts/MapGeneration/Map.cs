1e88dbc4450cb918ab906f88cf2f5d5876de6b0a
Marcin Zalewski <marcinzal24@gmail.com>
2022-01-14 16:21:46 +0100

    Generating hexagonal tilemap

---
+++ AStar/Assets/Scripts/MapGeneration/Map.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public List<Texture2D> textures;
+
+    [Header("Map Size")]
+    [SerializeField, Min(5)]
+    private uint width = 5;
+
+    [SerializeField, Min(5)]
+    private uint height = 5;
+
+    [Header("Hexagon Settings")]
+    [SerializeField]
+    private Transform hexagonPrefab = null;
+
+    [Tooltip("Can create a gap between hexagons")]
+    [SerializeField, Min(0f)]
+    private float haxagonGap = 0f;
+
+    // Used to get the right z coordinate for the tile
+    private const float hexagonHeightDiffrence = 0.75f;
+
+    private void Start()
+    {
+        CreateMap();
+    }
+
+    private void CreateMap()
+    {
+        uint[,] map = MapGenerator.GenerateMap(textures.Count, width, height);
+
+        // Create hexagon map
+        for (int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                Transform hexagon = Instantiate(hexagonPrefab, transform);
+                hexagon.transform.position = CalculateHexagonWorldPosition(x, y);
+            }
+        }
+    }
+
+    private Vector3 CalculateHexagonWorldPosition(int x, int y)
+    {
+        // Get x coordinate offset every second row, so the hexagon goes in the right place
+        float Xoffset = 0;
+        if(y % 2 != 0)
+        {
+            Xoffset = hexagonPrefab.localScale.y / 2;
+        }
+
+        // Calculate the hexagon position
+        Vector2 hexagonPosition = new Vector2(-width / 2 + x, -height / 2 + y);
+        hexagonPosition.x = hexagonPosition.x * (hexagonPrefab.localScale.x + haxagonGap) + Xoffset;
+        hexagonPosition.y = hexagonPosition.y * (hexagonPrefab.localScale.y + haxagonGap) * hexagonHeightDiffrence;
+
+        return new Vector3(hexagonPosition.x, 0, hexagonPosition.y);
+    }
+}
---
+++ AStar/Assets/Scripts/MapGeneration/MapGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MapGenerator
+{
+    /// <summary>
+    /// Generates a 2D uint array can can be used to create a map of hexagons
+    /// </summary>
+    /// <param name="texturesNum"> Amount of textures </param>
+    /// <param name="width"> Width of map </param>
+    /// <param name="height"> Height of map </param>
+    /// <returns> 2D uint array </returns>
+    public static uint[,] GenerateMap(int texturesNum, uint width, uint height)
+    {
+        uint[,] map = new uint[width, height];
+
+        // Fill the array
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                map[i, j] = 0;
+            }
+        }
+
+        return map;
+    }
+}
